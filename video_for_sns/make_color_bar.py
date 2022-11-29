e890a5c6e1655302bb297830e318e2ce5795dc9a
k-wakisaka <k.wakisaka@vaikobo.com>
2022-11-29 20:48:27 +0900

    first commit

---
+++ video_for_sns/make_color_bar.py
@@ -0,0 +1,23 @@
+from PIL import Image, ImageDraw, ImageFont
+import argparse
+import os
+
+
+def main(height, width, color, output_image_path):
+    os.makedirs(os.path.dirname(output_image_path), exist_ok=True)
+
+    image = Image.new("RGB", (width, height), color)
+    image.save(output_image_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='make string image')
+    parser.add_argument('--height', type=int, default=128)
+    parser.add_argument('--width', type=int, default=1280)
+    parser.add_argument('--color', type=str, default='white')
+    parser.add_argument('--output_image_path', type=str, default='./images/white_bar.png')
+    args = parser.parse_args()
+
+    args.output_image_path = os.path.expanduser(args.output_image_path)
+
+    main(args.height, args.width, args.color, args.output_image_path)
