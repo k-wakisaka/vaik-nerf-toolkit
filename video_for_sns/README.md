fbaaa9050d4ec61aa2dd88c6ff6f04aaafacb09b
k-wakisaka <k.wakisaka@vaikobo.com>
2022-11-30 13:30:17 +0900

    first commit

--- video_for_sns/README.md
+++ video_for_sns/README.md
@@ -8,3 +8,17 @@
     - Render icon
       - Capture Movie
 
+## convert pixel art
+
+```shell
+python convert_pixel_art.py --input_image_dir_path ~/home/kentaro/Desktop/VideoCaptures \
+                            --resize_sub_pixel 64 \
+                            --quantize_bit 1 \
+                            --output_image_dir_path ~/home/kentaro/Desktop/VideoCaptures_out
+```
+
+## merge widget
+
+```shell
+
+```
--- video_for_sns/make_string_image.py
+++ video_for_sns/make_string_image.py
@@ -3,15 +3,15 @@ import argparse
 import os
 
 
-def main(string, font_size, font_color, output_image_path):
+def main(string, font_size, font_color, output_image_path, font_name):
     os.makedirs(os.path.dirname(output_image_path), exist_ok=True)
 
     height = int(font_size * 1.2)
-    width = height * len(string) // 2
+    width = int(height * len(string) / 1.25)
     image = Image.new("RGB", (width, height), (0, 0, 0))
     draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), 'arial.ttf'), font_size)
+    font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), font_name), font_size)
     draw.text((font_size // 5, 0), string, fill=font_color, font=font)
 
     image.save(output_image_path)
@@ -19,12 +19,13 @@ def main(string, font_size, font_color, output_image_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='make string image')
-    parser.add_argument('--string', type=str, default='32bit')
+    parser.add_argument('--string', type=str, default='A. 三角')
     parser.add_argument('--font_size', type=int, default=100)
-    parser.add_argument('--font_color', type=str, default='coral')
-    parser.add_argument('--output_image_path', type=str, default='images/32bit_coral.png')
+    parser.add_argument('--font_color', type=str, default='red')
+    parser.add_argument('--output_image_path', type=str, default='images/answer_red.png')
+    parser.add_argument('--font_name', type=str, default='HannariMincho-Regular.otf')
     args = parser.parse_args()
 
     args.output_image_path = os.path.expanduser(args.output_image_path)
 
-    main(args.string, args.font_size, args.font_color, args.output_image_path)
+    main(args.string, args.font_size, args.font_color, args.output_image_path, args.font_name)
