9b63248193adedae6bb7fc9caa7812f781c1b083
k-wakisaka <k.wakisaka@vaikobo.com>
2022-12-10 00:44:12 +0900

    first model

--- ReflectionRemover/train.py
+++ ReflectionRemover/train.py
@@ -70,9 +70,9 @@ if __name__ == '__main__':
     parser.add_argument('--valid_input_dir_path', type=str, default='~/Desktop/ReflectionRemoveData/valid')
     parser.add_argument('--model_type', type=str, default='unet2d')
     parser.add_argument('--epochs', type=int, default=1000)
-    parser.add_argument('--step_size', type=int, default=500)
+    parser.add_argument('--step_size', type=int, default=250)
     parser.add_argument('--batch_size', type=int, default=8)
-    parser.add_argument('--test_max_sample', type=int, default=32)
+    parser.add_argument('--test_max_sample', type=int, default=8)
     parser.add_argument('--output_dir_path', type=str, default='~/Desktop/output_model')
     args = parser.parse_args()
 
