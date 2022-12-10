5f457f8dbe4e29132fcb1c599345e0293266f224
k-wakisaka <k.wakisaka@vaikobo.com>
2022-12-10 09:02:46 +0900

    first model

--- ReflectionRemover/image/models/test_model.py
+++ ReflectionRemover/image/models/test_model.py
@@ -9,6 +9,7 @@ from image.models import unet
 class TestUtils(unittest.TestCase):
     def test_simple(self):
         model = unet.prepare((512, 512, 3))
+        model.summary()
         output_model_path = os.path.expanduser('~/Desktop/unet_model.png')
         os.makedirs(os.path.dirname(output_model_path), exist_ok=True)
         tf.keras.utils.plot_model(model, output_model_path, show_shapes=True, show_layer_names=True, show_layer_activations=True)
--- ReflectionRemover/video/models/test_model.py
+++ ReflectionRemover/video/models/test_model.py
@@ -9,24 +9,28 @@ from video.models import simple, unet3d, unet2d, unet2d_tcn
 class TestUtils(unittest.TestCase):
     def test_simple(self):
         model = simple.prepare((8, 512, 512, 3))
+        model.summary()
         output_model_path = os.path.expanduser('~/Desktop/simple_model.png')
         os.makedirs(os.path.dirname(output_model_path), exist_ok=True)
         tf.keras.utils.plot_model(model, output_model_path, show_shapes=True, show_layer_names=True, show_layer_activations=True)
 
     def test_unet3d(self):
         model = unet3d.prepare((8, 512, 512, 3))
+        model.summary()
         output_model_path = os.path.expanduser('~/Desktop/unet3d_model.png')
         os.makedirs(os.path.dirname(output_model_path), exist_ok=True)
         tf.keras.utils.plot_model(model, output_model_path, show_shapes=True, show_layer_names=True, show_layer_activations=True)
 
     def test_unet2d(self):
         model = unet2d.prepare((8, 512, 512, 3))
+        model.summary()
         output_model_path = os.path.expanduser('~/Desktop/unet2d_model.png')
         os.makedirs(os.path.dirname(output_model_path), exist_ok=True)
         tf.keras.utils.plot_model(model, output_model_path, show_shapes=True, show_layer_names=True, show_layer_activations=True)
 
     def test_unet2d_tcn(self):
         model = unet2d_tcn.prepare((8, 512, 512, 3))
+        model.summary()
         output_model_path = os.path.expanduser('~/Desktop/unet2d_tcn_model.png')
         os.makedirs(os.path.dirname(output_model_path), exist_ok=True)
         tf.keras.utils.plot_model(model, output_model_path, show_shapes=True, show_layer_names=True, show_layer_activations=True)
